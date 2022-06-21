d8d49fbd800e5cd6f0fb600517ee9f0d27d68506
iljinjang <107455114+iljinjang@users.noreply.github.com>
2022-06-21 20:13:55 +0900

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable export_wordpress1_source_image_id { default = "ocid1.image.oc1.ap-seoul-1.aaaaaaaayi6fjg6fkbk7b6yfm2rg6wlayrdcley2tj4qydwpweiyueoh4xpa" }
+variable export_wordpress2_source_image_id { default = "ocid1.image.oc1.ap-seoul-1.aaaaaaaa52lnop5b6wgjbnbkqwf3rfuqxeytshrjnhea7oumxohi5n6mmgka" }
+variable region { default = "ap-seoul-1" }
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaabdf7cksfef5vrhbgv34hnjue2cm3p45zhbhe3vkct3bbtwtbruza" }
+variable export_BastionVM_source_image_id { default = "ocid1.image.oc1.ap-seoul-1.aaaaaaaayi6fjg6fkbk7b6yfm2rg6wlayrdcley2tj4qydwpweiyueoh4xpa" }
